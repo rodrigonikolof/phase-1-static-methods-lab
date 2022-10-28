2044b3082b3bc3c1b7ba29ce9cebc9f98259dfe0
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-10-29 09:58:21 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 class Formatter {
-  //add static methods here
+  static capitalize (string){
+    return string[0].toUpperCase() + string.substring(1);
+  }
+  static sanitize(string){
+    return  string.replace(/[^a-zA-Z0-9 '-]/gmi, "")
+  }
+  static titleize (string){
+    let stringHelper = string.split(" ");
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from'];
+    let finalString = stringHelper.map((word,index) => index !==0 && exceptions.includes(word) ? word : Formatter.capitalize(word))
+
+    return finalString.join(" ")
+  }
 }
+
